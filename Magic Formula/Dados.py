0ca321f2174f8894e168c449ffebc79f222d9897
GilcimarMoraes <myrraws@yahoo.com.br>
2024-03-11 21:59:57 -0300

    comentários adicionados

--- Magic Formula/Dados.py
+++ Magic Formula/Dados.py
@@ -1,35 +1,57 @@
+# Importação das bibliotecas necessárias
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import yfinance as yf
 
+# URL do site Fundamentus
 url = 'https://fundamentus.com.br/resultado.php'
+
+# Configuração do cabeçalho HTTP
 data = {"User-Agent":"Mozzila/5.0"}
+
+# Requisição GET para obter o conteúdo HTML da página
 response = requests.get(url, headers=data)
-soup = BeautifulSoup(response.text,'lxml')
+
+# Criação de um objeto BeautifulSoup para analisar o HTML
+soup = BeautifulSoup(response.text, 'lxml')
+
+# Extração da tabela de dados da página
 tabela = soup.find_all('table')[0]
-df = pd.read_html(str(tabela), decimal = ',', thousands= '.')[0]
 
-df.to_excel('dadosCompletosFundamentus.xlsx', index = False, engine = 'openpyxl')
+# Leitura da tabela HTML para um DataFrame do Pandas
+df = pd.read_html(str(tabela), decimal=',', thousands='.')[0]
 
+# Salvando o DataFrame como um arquivo Excel
+df.to_excel('dadosCompletosFundamentus.xlsx', index=False, engine='openpyxl')
+
+# Filtragem dos dados com liquidez mínima
 liquidezMinima = df[df['Liq.2meses'] > 500000]
 
+# Filtragem dos dados com patrimônio líquido positivo
 PatrimonioLiquido = liquidezMinima[liquidezMinima['Patrim. Líq'] > 0]
 
+# Seleção de colunas relevantes
 dataNovo = PatrimonioLiquido[['Papel', 'ROIC', 'EV/EBIT']]
 
-EvOrdenado = dataNovo.sort_values(by = 'EV/EBIT')
-EvOrdenado = EvOrdenado.reset_index(drop = True)
+# Ordenação dos dados por EV/EBIT
+EvOrdenado = dataNovo.sort_values(by='EV/EBIT')
+EvOrdenado = EvOrdenado.reset_index(drop=True)
 EvOrdenado['EvOrdenado'] = EvOrdenado.index
 
-RoicOrdenado = EvOrdenado.sort_values(by = 'ROIC', ascending = False)
-RoicOrdenado = RoicOrdenado.reset_index(drop = True)
+# Ordenação dos dados por ROIC
+RoicOrdenado = EvOrdenado.sort_values(by='ROIC', ascending=False)
+RoicOrdenado = RoicOrdenado.reset_index(drop=True)
 RoicOrdenado['RoicOrdenado'] = RoicOrdenado.index
+
+# DataFrame final com dados ordenados
 dados = RoicOrdenado
 
+# Cálculo da métrica "magic"
 dados['magic'] = dados['EvOrdenado'] + dados['RoicOrdenado']
-dados.sort_values(by = 'magic')
 
-carteira = dados.sort_values(by = 'magic').head(30)
+# Ordenação dos dados por "magic" e seleção dos primeiros 30
+carteira = dados.sort_values(by='magic').head(30)
 
+# Impressão da carteira
 print(carteira)
